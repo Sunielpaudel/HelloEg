3dd359ad8670e54dc2b98342263b82e8fbb614d1
Suniel <Suniel.paudel2047@gmail.com>
2024-09-26 11:21:16 -0700

    added sum functions.

--- Hello/tryEg.cpp
+++ Hello/tryEg.cpp
@@ -4,8 +4,15 @@ using namespace std;
 void hello() {
 	cout << "Hello World" << endl;
 }
+int sum(int num1, int num2) {
+	int num3 = num1 + num2;
+
+	return num3;
+}
 
 int main() {
+
+	cout<<sum(3, 5)<<endl;
 
 	hello();
 }
